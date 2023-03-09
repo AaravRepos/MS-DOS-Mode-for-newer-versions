198c5d8a65bdd8dc90f4427fe6be468219734760
AaravKrishanChugh65 <103321550+AaravKrishanChugh65@users.noreply.github.com>
2023-03-08 19:15:37 -0500

    Update download-winxeditor.zip.cmd

--- download-winxeditor.zip.cmd
+++ download-winxeditor.zip.cmd
@@ -5,4 +5,5 @@ echo Okay. Starting download.
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 echo This will download in the folder this is in.
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+pause
 powershell -Command "Invoke-WebRequest https://github.com/AaravRepos/MS-DOS-Mode-for-newer-versions/releases/download/1.0.1/WinXEditor.zip -OutFile WinXEditor.zip"
